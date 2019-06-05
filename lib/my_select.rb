d283b208f452c9f84eb2a1dfa93bcf45c6841c93
Nina Kazzim <kazzim@icloud.com>
2019-06-05 03:21:26 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  size = arr.size
+  count = 0
+  arr2 = []
+
+  while count < size
+    ele = arr[count]
+
+    if yield(ele) == true
+      arr2 << ele
+    end
+    count += 1
+  end
+  return arr2
 end
